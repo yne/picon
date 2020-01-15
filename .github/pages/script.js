8df0f048a8755ad872cf9159a8eab2150c378b1f
Rémy F <yne@users.noreply.github.com>
2020-01-15 17:55:30 +0100

    template.html

--- .github/pages/script.js
+++ .github/pages/script.js
@@ -12,26 +12,31 @@ function toPath(path,str){
 	else path.lineTo(...normalize(str));
 	return path;
 }
+function blankGlyph(unicode){
+	return new opentype.Glyph({name:unicode, unicode, advanceWidth:16, path: new opentype.Path()})
+}
 function makeFont(form, advanceWidth=16, familyName="picon", styleName="medium"){
 	if(typeof opentype === "undefined")
 		return document.body.append(Object.assign(document.createElement('script'),{
 			src:'https://cdn.jsdelivr.net/npm/opentype.js',
 			onload: ()=>makeFont(...arguments)
 		}));
-	const svgs = [...form.querySelectorAll('.check img')].map(path=>[path.closest('li').id, path.src.slice(20+67,-15)]);
+	const checkdGlyph = [...form.querySelectorAll('.check img')].map(path=>[path.closest('li').id.replace(/^latin-/,''), path.src.slice(20+67,-15)]);
 	const notdefGlyph = new opentype.Glyph({name: '.notdef', unicodes: 0,advanceWidth, path: new opentype.Path()});
-	const alphaGlyphs = [];/*"-abcdefghijklmnopqrstuvwxyz0123456789".split('').map(name=>new opentype.Glyph({
-		name, unicode: name.charCodeAt(0), advanceWidth, path: new opentype.Path()}));*/
+	const alphaGlyphs = "-abcdefghijklmnopqrstuvwxyz0123456789".split('').filter(c=>!checkdGlyph.find(([C])=>c==C)).map(c=>[c,""]);
+	// fill required glyph (for ligature) with blank glyph (if not in the checkdGlyph)
+	const svgs = [...alphaGlyphs, ...checkdGlyph];
 	var ligatures = [];
-	//TODO load alphaglyph if name starts with latin-
-	const glyphs = [notdefGlyph,...alphaGlyphs, ...svgs.map((svg,i) => {
+	const glyphs = [notdefGlyph, ...svgs.map((svg,i) => {
 		const [name,uni] = svg[0].split('.');
-		const unicode = (!name&&uni&&uni.length==1)?uni.charCodeAt(0):0xE000+i;
+		const c = name.charCodeAt(0);
+		const unicode = (name.length==1 && c < 255) ? c : 0xE000+i;
 		const path = svg.slice(1).reduce(toPath,new opentype.Path());
-		const glyph = new opentype.Glyph({name:name||uni, unicode, advanceWidth, path});
+		const glyph = new opentype.Glyph({name, unicode, advanceWidth, path});
 		if(name)ligatures.push({sub: name, by:glyph});
 		return glyph;
 	})]
+	console.log(svgs,glyphs)
 	const fontMetrics = {unitsPerEm: advanceWidth, ascender: advanceWidth, descender: -Number.EPSILON};
 	const font = new opentype.Font({familyName, styleName,...fontMetrics, glyphs});
 	ligatures.forEach(({sub,by}) => {
@@ -39,12 +44,13 @@ function makeFont(form, advanceWidth=16, familyName="picon", styleName="medium")
 			sub:sub.split('').map(e=>e.charCodeAt(0)).map(unicode=>glyphs.findIndex(g=>g.unicode==unicode)),
 			by:glyphs.indexOf(by)
 		}
-		console.log(liga.sub, liga.by);
+		//console.log(liga.sub, liga.by);
 		font.substitution.add("liga", liga);
 	});
 	document.fonts.add(new FontFace(font.names.fontFamily.en, font.toArrayBuffer()));
 	console.log(font);
-	font.download();
+	applyFont(font);
+	//font.download();
 }
 
 function filter(form,className="highlight") {
@@ -89,3 +95,8 @@ function clip(a, event) {
 		res => a.classList.add('copied'),
 		err => alert(`Unable to copy ${text}\n`+err));
 }
+/*
+document.addEventListener('DOMContentLoaded', () =>{
+	document.querySelectorAll('li[id^=latin-]').forEach(el=>el.classList.add('check'));
+	makeFont(List)
+})*/
