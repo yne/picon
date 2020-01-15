1e76dccf9a350a2cc357c5c81f1d17bfd4b7367e
yne <yne@users.noreply.github.com>
2020-01-15 01:08:30 +0100

    TODO: make ligature work again

--- .github/pages/script.js
+++ .github/pages/script.js
@@ -18,11 +18,12 @@ function makeFont(form, advanceWidth=16, familyName="picon", styleName="medium")
 			src:'https://cdn.jsdelivr.net/npm/opentype.js',
 			onload: ()=>makeFont(...arguments)
 		}));
-	const svgs = [...form.querySelectorAll(':checked+svg')].map(path=>[path.closest('a').id, path.getAttribute('d')]);
+	const svgs = [...form.querySelectorAll('.check img')].map(path=>[path.closest('li').id, path.src.slice(20+67,-15)]);
 	const notdefGlyph = new opentype.Glyph({name: '.notdef', unicodes: 0,advanceWidth, path: new opentype.Path()});
 	const alphaGlyphs = [];/*"-abcdefghijklmnopqrstuvwxyz0123456789".split('').map(name=>new opentype.Glyph({
 		name, unicode: name.charCodeAt(0), advanceWidth, path: new opentype.Path()}));*/
 	var ligatures = [];
+	//TODO load alphaglyph if name starts with latin-
 	const glyphs = [notdefGlyph,...alphaGlyphs, ...svgs.map((svg,i) => {
 		const [name,uni] = svg[0].split('.');
 		const unicode = (!name&&uni&&uni.length==1)?uni.charCodeAt(0):0xE000+i;
@@ -33,44 +34,58 @@ function makeFont(form, advanceWidth=16, familyName="picon", styleName="medium")
 	})]
 	const fontMetrics = {unitsPerEm: advanceWidth, ascender: advanceWidth, descender: -Number.EPSILON};
 	const font = new opentype.Font({familyName, styleName,...fontMetrics, glyphs});
-	ligatures.forEach(({sub,by}) => font.substitution.add("liga",{
-		sub:sub.split('').map(e=>e.charCodeAt(0)).map(unicode=>glyphs.findIndex(g=>g.unicode==unicode)),
-		by:glyphs.indexOf(by)
-	}) );
+	ligatures.forEach(({sub,by}) => {
+		const liga = {
+			sub:sub.split('').map(e=>e.charCodeAt(0)).map(unicode=>glyphs.findIndex(g=>g.unicode==unicode)),
+			by:glyphs.indexOf(by)
+		}
+		console.log(liga.sub, liga.by);
+		font.substitution.add("liga", liga);
+	});
 	document.fonts.add(new FontFace(font.names.fontFamily.en, font.toArrayBuffer()));
 	console.log(font);
+	font.download();
 }
 
 function filter(form,className="highlight") {
-	form.querySelectorAll('.'+className).forEach(e=>e.classList.remove(className))
-	form.querySelector("blockquote").hidden=!form.q.value;
-	if(form.q.value)
-		form.match.value=[...form.querySelectorAll(`a[id*='${form.q.value}']`)].map(e=>e.classList.add(className)).length;
+	const all = form.querySelectorAll('li');
+	all.forEach(li=>li.hidden=false);
+	form.querySelector("blockquote").hidden=!form.q.value
+	if(!form.q.value)return;
+	const rule = `li:not([id*='${form.q.value}'])`;
+	const matched = form.querySelectorAll(rule)
+	matched.forEach(li=>li.hidden=true)
+	form.match.value = all.length - matched.length;
+	
+}
+function updateCheck(form){
+	const nbChecked = form.querySelectorAll('.check').length
+	form.check.value=nbChecked
+	form.check.parentElement.hidden=!nbChecked
 }
 function highlightCheck(form, checked){
-	form.querySelectorAll('.highlight [type=checkbox]').forEach(e=>e.checked=checked)
+	const list = form.querySelectorAll('li:not([hidden])')
+	list.forEach(li=>li.classList=checked?"check":'')
+	updateCheck(form)
 }
 let prevToggle=null;
-function toggle(checkbox, event){
-	const all = [...checkbox.form.querySelectorAll('a>[type=checkbox]')];
-	if(event.shiftKey && prevToggle) {
-		const [from, to] = [prevToggle,checkbox].map(c=>all.findIndex(e=>c==e)).sort();
-		all.slice(from+1,to).map(cb=>cb.checked^=1);
-	}
-	const checked = checkbox.form.querySelectorAll(':checked').length
-	checkbox.form.check.value=checked
-	checkbox.form.fab.hidden=!checked
-	prevToggle = checkbox;
+function toggle(el, event){
+	const form = el.closest('form');
+	const li = el.closest('li');
+	const all = [...form.querySelectorAll('li')];
+	if (!prevToggle)prevToggle = li;
+	const [from, to] = [prevToggle,li].map(c=>all.findIndex(e=>c==e));
+	var els = event.shiftKey?all.slice(...(from > to ? [to,from] : [from+1,to+1])):[li];
+	els.map(cb=>cb.classList.toggle("check"))
+	prevToggle = li;
+	updateCheck(form)
 }
 function clip(a, event) {
-	//event.stopPropagation();//avoid parent <a> href follow
-	event.preventDefault();
-	const text = a.id;
+	const text = a.innerText;
   if (!navigator.clipboard)
 		return alert(`No clipboard API to copy ${text}`);
 	setTimeout(() => a.classList.remove('copied'),1000);
-	navigator.clipboard.writeText(a.id).then(
+	navigator.clipboard.writeText(text).then(
 		res => a.classList.add('copied'),
 		err => alert(`Unable to copy ${text}\n`+err));
-	return toggle(a.querySelector('[type=checkbox]'),event);
 }
