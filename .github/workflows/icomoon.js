49e0d1af28e46ac067309ae04b12ea08d654e36e
Rémy F <yne@users.noreply.github.com>
2021-02-27 12:36:52 +0100

    update github action

--- .github/workflows/icomoon.js
+++ .github/workflows/icomoon.js
@@ -1,57 +1,57 @@
 /**
 DESCRIPTION
-  Generate a icomoon-compatible JSON file from a metadata.json and a list of (carefully named) svgs
+	Generate a icomoon-compatible JSON file from a metadata.json and a list of (carefully named) svgs
 USAGE
-  icomoon.js metadata.json *.svg
+	icomoon.js metadata.json *.svg
 INPUT
-  home.#building.#house.svg
-  %2f.#latin.svg
+	home.#building.#house.svg
+	%2f.#latin.svg
 OUTPUT
 [
 	"icon": {
 		"paths": [
-			"M512 32l-512 512 96 96 96-96v416h256v-192h128v192h256v-416l96 96 96-96-512-512zM512 448c-35.346 0-64-28.654-64-64s28.654-64 64-64c35.346 0 64 28.654 64 64s-28.654 64-64 64z"
+			"M1,8L3,8L3,5L5,5L5,8L7,8L7,4L8,4L4,0L0,4L1,4"
 		],
 		"tags": [
 			"building",
 			"house"
 		],
 		"defaultCode": 58880,
-		"grid": 16
+		"grid": 8
 	},
 	"properties": {
-		"ligatures": "home2, house2",
-		"name": "home2"
+		"ligatures": "home",
+		"name": "home"
 	},
 	...
 ]
 **/
-const log = (str) => (console.error(str),str)
-process.stdout.write(JSON.stringify(((metadata,...svgs) => ({
+const log = (str) => (console.error(str), str);
+process.stdout.write(JSON.stringify(((metadata, ...svgs) => ({
 	IcoMoonType: "selection",
-	height: 8,
+	height: 32, // preview size
 	metadata: JSON.parse(require('fs').readFileSync(metadata)),
 	icons: svgs.map(p => ({
-			props: require('path').basename(p, '.svg').split('.'),
-			data: require('fs').readFileSync(p, 'utf8')
-		}))
-		.map(({props, data}, idx) => ({
-			_unicodes: props.filter(p=>p.startsWith('%')).map(xx=>decodeURIComponent(xx).charCodeAt(0)),
+		props: require('path').basename(p, '.svg').split('.'),
+		data: require('fs').readFileSync(p, 'utf8')
+	}))
+		.map(({ props, data }, idx) => ({
+			_unicodes: props.filter(p => p.startsWith('%')).map(xx => decodeURIComponent(xx).charCodeAt(0)),
 			icon: {
-				tags: props.filter(p=>p.startsWith('#')).map(h=>h.slice(1)),
-				grid: +(data.match(/ viewBox="\d+ \d+ \d+ (\d+)"/)||[16]).slice(-1)[0],
-				paths: [...data.matchAll(/ d="(.*?)"/g)].map(d=>d[1])
+				tags: props.filter(p => p.startsWith('#')).map(h => h.slice(1)),
+				grid: +(data.match(/ viewBox="\d+ \d+ \d+ (\d+)"/) || [16]).slice(-1)[0],
+				paths: [...data.matchAll(/ d="(.*?)"/g)].map(d => d[1])
 			},
 			properties: {
-				ligatures: props.filter(p=>p.match(/^[a-z]/)).join(', '),
-				name: props[0].startsWith('%') ? null : props[0].replace(/^\d/,s=>'_'+s)
+				ligatures: props.filter(p => p.match(/^[a-z]/)).join(', '),
+				name: props[0].startsWith('%') ? null : props[0].replace(/^\d/, s => '_' + s)
 			}
 		}))
 		// iconmoon json expect only 1 unicode per glyph
 		// split our unicodes into multiple glyphs with .defaultCode:int
-		.map(({_unicodes, icon, properties}, idx) =>
-			_unicodes.length ? _unicodes.map(u => ({icon:{defaultCode:u, ...icon},properties})) : [{icon:{defaultCode:0xE000+idx, ...icon}, properties}]
-		).flat().map(el => ({...el, properties:{...el.properties, name: el.properties.name || 'uni'+('0000'+el.icon.defaultCode.toString(16).toUpperCase()).slice(-4)}}))
+		.map(({ _unicodes, icon, properties }, idx) =>
+			_unicodes.length ? _unicodes.map(u => ({ icon: { defaultCode: u, ...icon }, properties })) : [{ icon: { defaultCode: 0xE000 + idx, ...icon }, properties }]
+		).flat().map(el => ({ ...el, properties: { ...el.properties, name: el.properties.name || 'uni' + ('0000' + el.icon.defaultCode.toString(16).toUpperCase()).slice(-4) } }))
 		.sort((a, b) => a.icon.defaultCode - b.icon.defaultCode)
-}))(...process.argv.slice(2)), null, 2))
+}))(...process.argv.slice(2)), null, 2));
 
--- Makefile
+++ Makefile
@@ -1,19 +1,17 @@
-all:  solid/solid.min.woff2 solid/solid.all.woff2 solid/solid.all.ttf solid/solid.all.otf solid/solid.min.otf solid/solid.tar.bz2 solid/solid.all.json
-#clean:; rm -f solid/solid*
-# intermediate "target : requirements" rules
+all: solid.min.woff2 solid.tar.bz2 # solid.all.woff2 solid.all.ttf solid.all.otf solid.tar.bz2 solid.all.json
+clean:; rm -f *.ttf *.otf *.woff2 *.pb *.json *.tar.bz2
+
 %.ttf: %.n.ttf ; ttfautohint $^ $@
 %.n.ttf: %.otf ; fontforge -lang=ff -c 'Open("$^");Generate("$@")'
 %.woff2: %.otf ; woff2_compress $^
-%.tar.bz2:     ; tar cj --transform 's/#.*/svg/' $(dir $@)*.svg > $@
-%.all.json:    ; node .github/workflows/icomoon.js $(dir $@)manifest.json $(dir $@)*.*.svg > $@
+%.tar.bz2: %   ; tar cj --transform='s|[.].*|.svg|;s|^.*/||' $^/*.svg > $@
+%.all.json: %  ; node .github/workflows/icomoon.js $^/manifest.json $^/*.svg > $@
 %.min.json: %.all.json ; jq 'del(.icons[]|select(.icon.tags|index("latin")))' <$^ > $@
 %.otf: %.json .github/pages/opentype.js ; node .github/pages/build.js <$< >$@
 %.pb: %.pb.m4  ; m4 -D JSONS="($(shell ls -m */manifest.json))" <$^ >$@
 
 # external deps
 .github/pages/opentype.js:; wget https://cdn.jsdelivr.net/npm/opentype.js@1.3.3/dist/opentype.js -O $@
-
-#bottom right left down up %c2%b0 fast
 
 # pages target
 %.htm: %.md   ; curl -s -HContent-Type:text/x-markdown https://api.github.com/markdown/raw --data-binary @- <$^ | sed s/user-content-// >$@
